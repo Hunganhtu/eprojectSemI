48d0f20c273be3644540b3e93cf0c2f8db5e79fc
tuananhpv0210 <tuananhpv0210@gmail.com>
2020-06-25 08:23:28 +0700

    them moi cua tadz

--- app/Http/Controllers/Admin/BannerController.php
+++ app/Http/Controllers/Admin/BannerController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Banner;
 
 class BannerController extends Controller
 {
     //
+    public function store(Banner $banner)
+    {
+    	$banner->add();
+    	return 
+    }
 }
--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Banner extends Model
 {
     //
+    protected $fillable=['name','slug','image','like','status','order'];
+
+    public function add()
+    {
+    	
+    }
 }
